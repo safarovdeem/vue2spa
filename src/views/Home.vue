612157b5b264611fe9c35c3138c13a4f5cb32fed
safarovdeem <safarovdeem@yandex.com>
2021-03-30 09:31:19 +0500

    vue2 SPA files

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsumv</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
